c83b67f010985faa60abae1393e4b4ce1e5a2dea
Phil Mass <philrmass@gmail.com>
2021-11-10 20:31:17 -0800

    complete basic file server

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+const fs = require('fs');
 const Koa = require('koa');
 const KoaRouter = require('@koa/router');
 
 const port = 4445;
-const nirvanaPath = '/Users/philmass/Music/Library/Nirvana/In Utero/12 All Apologies.mp3';
 
 async function run() {
   console.log('Starting koa server');
@@ -21,55 +21,24 @@ run();
 async function getSong(ctx) {
   const guid = ctx.params.guid;
   const path = getSongPath(guid);
+  console.log(`[guid] = [${path}]`);
 
-  console.log('GUID', guid, 'PATH', path);
-
-  ctx.body = 'This is your song';
+  try {
+    if (fs.existsSync(path)) {
+      console.log('FILE-EXISTS');
+      ctx.body = fs.createReadStream(path);
+      ctx.attachment(path);
+    } else {
+      ctx.throw(400, `Song not found [${guid}]`);
+    }
+  } catch (err) {
+    ctx.throw(500, err);
+  }
 };
 
 function getSongPath(guid) {
   switch (guid) {
     case 'nirvana':
-      return nirvanaPath;
+      return '/Users/philmass/Music/Library/Nirvana/In Utero/12 All Apologies.mp3';
   }
 }
-
-
-/*
-router.get('/downloads/:version/:file', async function(ctx) {
-  const fileName = `${__dirname}/downloads/${ctx.params.version}/${ctx.params.file}`;
-  try {
-    if (fs.existsSync(fileName)) {
-      ctx.body = fs.createReadStream(fileName);
-      ctx.attachment(fileName);
-    } else {
-      ctx.throw(400, "Requested file not found on server");
-    }
-  } catch(error) {
-    ctx.throw(500, error);
-  }  
-});
-*/
-
-
-/*
-
-
-app.listen(3000);
-*/
-
-/*
-// app.js
-const Koa = require('koa');
-
-// Create Koa app
-const app = new Koa();
-
-// Serve requests, here, printing out a simple greeting
-app.use(async ctx => {
-    ctx.body = 'Hello World';
-});
-
-// Start the server
-app.listen(3000);
-*/
