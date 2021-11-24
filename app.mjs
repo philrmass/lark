c6f26cd9ccff6bbf31eb2ef7c79ae16af0983ba2
Phil Mass <philrmass@gmail.com>
2021-11-23 20:58:41 -0800

    add song routes

--- app.mjs
+++ app.mjs
@@ -7,7 +7,7 @@ import { initLibrary, parseArtists } from './utilities/library.mjs';
 
 const port = 4445;
 let songsByGuid = {};
-let artists = {};
+let artists = [];
 
 async function run() {
   const start = Date.now();
@@ -22,13 +22,15 @@ async function run() {
   }
 
   artists = parseArtists(songsByGuid);
-  console.log(`Parsed ${Object.keys(artists).length} artists in ${toSecs(start, Date.now())} seconds`);
+  console.log(`Parsed ${artists.length} artists in ${toSecs(start, Date.now())} seconds`);
 
   console.log('Starting koa server');
   const app = new Koa();
 
   var router = KoaRouter();
-  router.get('/songs/:guid', getSong);
+  router.get('/songs', getSongs);
+  router.get('/songs/:path', getSong);
+  router.get('/artists', getArtists);
 
   console.log(`Adding routes, listening on port ${port}`);
   app.use(router.routes());
@@ -40,44 +42,8 @@ function toSecs(start, end) {
   return secs.toFixed(3);
 }
 
-async function getSong(ctx) {
-  const guid = ctx.params.guid;
-  const path = getSongPath(guid);
-  console.log(`[${guid}] = [${path}]`);
-
-  try {
-    if (fs.existsSync(path)) {
-      ctx.body = fs.createReadStream(path);
-      //ctx.attachment(path);
-    } else {
-      ctx.throw(400, `Song not found [${guid}]`);
-    }
-  } catch (err) {
-    ctx.throw(500, err);
-  }
-};
-
-function getSongPath(guid) {
-  switch (guid) {
-    case 'nirvana.mp3':
-      return '/Users/philmass/Music/Library/Nirvana/In Utero/12 All Apologies.mp3';
-    default:
-      return null;
-  }
-}
-
-run();
-
+//??? reload
 /*
-var express = require('express');
-const lib = require('../utilities/library.js');
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-  const artists = global.artists || [];
-  res.send(JSON.stringify(artists));
-});
-
 router.post('/', async function(req, res, next) {
   const reload = req.body.reloadMetadata;
   if (reload) {
@@ -89,34 +55,40 @@ router.post('/', async function(req, res, next) {
     res.send(JSON.stringify(artists));
   }
 });
-
-router.get('/:artist', function(req, res, next) {
-  const artist = global.artists && global.artists[req.params.artist] || {};
-  res.send(JSON.stringify(artist));
-});
-
-module.exports = router;
 */
 
-/*
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-  const songsByGuid = global.songsByGuid || {};
-  res.send(JSON.stringify(Object.values(songsByGuid)));
-});
-
-router.get('/:path', function(req, res, next) {
-  console.log(` Load '${getSongDescription(req.params.path)}'`);
-  res.download(req.params.path);
-});
-
-function getSongDescription(path) {
-  const parts = path.split('\\');
-  const last = parts.slice(-3);
-  return last.join(':');
+async function getSongs(ctx) {
+  try {
+    ctx.body = JSON.stringify(songsByGuid);
+    ctx.response.set('content-type', 'application/json');
+  } catch (err) {
+    ctx.throw(500, err);
+  }
 }
 
-module.exports = router;
-*/
+async function getSong(ctx) {
+  const path = decodeURI(ctx.params.path);
+  console.log(`[${path}]`);
+
+  try {
+    if (fs.existsSync(path)) {
+      ctx.body = fs.createReadStream(path);
+      ctx.attachment(path);
+    } else {
+      ctx.throw(400, `Song not found [${path}]`);
+    }
+  } catch (err) {
+    ctx.throw(500, err);
+  }
+};
+
+async function getArtists(ctx) {
+  try {
+    ctx.body = JSON.stringify(artists);
+    ctx.response.set('content-type', 'application/json');
+  } catch (err) {
+    ctx.throw(500, err);
+  }
+}
+
+run();
