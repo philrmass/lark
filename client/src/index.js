0a04d7d91d26a6b784437b8a55cdd3133efab841
Phil Mass <philrmass@gmail.com>
2021-11-25 21:28:59 -0800

    add webpack, babel, css, react

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles/index.css';
+
+ReactDOM.render(
+  <div>Hello</div>,
+  document.getElementById('app'),
+);
+
+console.log('Lark is running');
---
+++ client/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+        include: /\.module\.css$/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /\.module\.css$/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js'],
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  plugins: [new ESLintPlugin()],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 4446,
+  },
+  infrastructureLogging: {
+    level: 'error',
+  },
+};
